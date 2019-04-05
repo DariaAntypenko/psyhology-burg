aeec2bc2d8137f2113d424cfe9f78f51e2713286
Антипенко Дарья <d.antipenko@ds.local>
2019-04-05 17:51:57 +0300

    05.04 17 51

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//accordion FAQ
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.collapsible');
+//     var instances = M.Collapsible.init(elems, options);
+// });
+
+// Or with jQuery
+
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+});
+
+//accordion FAQ
